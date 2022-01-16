4a9ac16d960d919c778a966a3d56d07803761a04
ArthurSonzogni <sonzogniarthur@gmail.com>
2022-01-16 20:50:46 +0100

    Table view
    This implements and resolves:
    https://github.com/ArthurSonzogni/json-tui/issues/14

---
+++ src/button.cpp
@@ -0,0 +1,57 @@
+#include <ftxui/component/captured_mouse.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/event.hpp>
+
+using namespace ftxui;
+
+Component MyButton(const char* prefix,
+                   const char* title,
+                   std::function<void()> on_click) {
+  class Impl : public ComponentBase {
+   public:
+    Impl(const char* prefix, const char* title, std::function<void()> on_click)
+        : prefix_(prefix), title_(title), on_click_(on_click) {}
+
+    // Component implementation:
+    Element Render() override {
+      auto style = Focused() ? (Decorator(inverted) | focus) : nothing;
+      return hbox({
+          text(prefix_),
+          text(title_) | style | color(Color::GrayDark) | reflect(box_),
+      });
+    }
+
+    bool OnEvent(Event event) override {
+      if (event.is_mouse() && box_.Contain(event.mouse().x, event.mouse().y)) {
+        if (!CaptureMouse(event))
+          return false;
+
+        TakeFocus();
+
+        if (event.mouse().button == Mouse::Left &&
+            event.mouse().motion == Mouse::Pressed) {
+          on_click_();
+          return true;
+        }
+
+        return false;
+      }
+
+      if (event == Event::Return) {
+        on_click_();
+        return true;
+      }
+      return false;
+    }
+
+    bool Focusable() const final { return true; }
+
+   private:
+    std::function<void()> on_click_;
+    const char* prefix_;
+    const char* title_;
+    Box box_;
+  };
+
+  return Make<Impl>(prefix, title, std::move(on_click));
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,27 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/table.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/string.hpp>
 #include <iostream>
 #include <nlohmann/json.hpp>
-
+#include "button.hpp"
 #include "mytoggle.hpp"
 #include "version.hpp"
 
 using JSON = nlohmann::json;
 using namespace ftxui;
+
+bool IsArrayOfObject(const JSON& json) {
+  if (!json.is_array())
+    return false;
+  for (const auto& element : json.items()) {
+    if (!element.value().is_object())
+      return false;
+  }
+  return true;
+}
 
 Component Indentation(Component child) {
   return Renderer(child, [child] {
@@ -45,7 +56,15 @@ Component FromObject(Component prefix,
                      const JSON& json,
                      bool is_last,
                      int depth);
+Component FromArrayAny(Component prefix,
+                       const JSON& json,
+                       bool is_last,
+                       int depth);
 Component FromArray(Component prefix,
+                    const JSON& json,
+                    bool is_last,
+                    int depth);
+Component FromTable(Component prefix,
                     const JSON& json,
                     bool is_last,
                     int depth);
@@ -70,7 +89,7 @@ Component From(const JSON& json, bool is_last, int depth) {
   if (json.is_object())
     return FromObject(Empty(), json, is_last, depth);
   if (json.is_array())
-    return FromArray(Empty(), json, is_last, depth);
+    return FromArrayAny(Empty(), json, is_last, depth);
   if (json.is_string())
     return FromString(json, is_last);
   if (json.is_number())
@@ -130,7 +149,7 @@ Component FromKeyValue(const std::string& key,
     if (value.is_object())
       return FromObject(prefix, value, is_last, depth);
     else
-      return FromArray(prefix, value, is_last, depth);
+      return FromArrayAny(prefix, value, is_last, depth);
   }
 
   auto child = From(value, is_last, depth);
@@ -143,17 +162,32 @@ Component FromKeyValue(const std::string& key,
   });
 }
 
+Component FromArrayAny(Component prefix,
+                       const JSON& json,
+                       bool is_last,
+                       int depth) {
+  class Impl : public ComponentBase {
+   public:
+    Impl(Component prefix, const JSON& json, bool is_last, int depth) {
+      Add(FromArray(prefix, json, is_last, depth));
+    }
+  };
+
+  return Make<Impl>(prefix, json, is_last, depth);
+}
+
 Component FromArray(Component prefix,
                     const JSON& json,
                     bool is_last,
                     int depth) {
   class Impl : public ComponentBase {
    public:
-    Impl(Component prefix, const JSON& json, bool is_last, int depth) {
+    Impl(Component prefix, const JSON& json, bool is_last, int depth)
+        : prefix_(prefix), json_(json), is_last_(is_last), depth_(depth) {
       is_expanded_ = (depth <= 0);
       auto children = Container::Vertical({});
-      int size = static_cast<int>(json.size());
-      for (auto& it : json.items()) {
+      int size = static_cast<int>(json_.size());
+      for (auto& it : json_.items()) {
         bool is_last = --size == 0;
         children->Add(Indentation(From(it.value(), is_last, depth + 1)));
       }
@@ -164,15 +198,134 @@ Component FromArray(Component prefix,
         children->Add(Renderer([] { return text("],"); }));
 
       auto toggle = MyToggle("[", is_last ? "[...]" : "[...],", &is_expanded_);
+
+      auto upper = Container::Horizontal({
+          FakeHorizontal(prefix_, toggle),
+      });
+
+      // Turn this array into a table.
+      if (IsArrayOfObject(json)) {
+        auto expand_button = MyButton("   ", "(table view)", [this] {
+          auto* parent = Parent();
+          auto replacement = FromTable(prefix_, json_, is_last_, depth_);
+          parent->DetachAllChildren();  // Detach this.
+          parent->Add(replacement);
+        });
+
+        upper = Container::Horizontal({upper, expand_button});
+      }
+
       Add(Container::Vertical({
-          FakeHorizontal(prefix, toggle),
+          upper,
           Maybe(children, &is_expanded_),
       }));
     }
+    Component prefix_;
+    const JSON& json_;
+    bool is_last_;
+    int depth_;
+
+    Component tab_;
+    int tab_index_ = 0;
     bool is_expanded_ = false;
   };
   return Make<Impl>(prefix, json, is_last, depth);
 }
+
+Component FromTable(Component prefix,
+                    const JSON& json,
+                    bool is_last,
+                    int depth) {
+  class Impl : public ComponentBase {
+   public:
+    Impl(Component prefix, const JSON& json, bool is_last, int depth)
+        : prefix_(prefix), json_(json), is_last_(is_last), depth_(depth) {
+      std::vector<Component> components;
+
+      // Turn this array into a table.
+      expand_button_ = MyButton("", "(array view)", [this] {
+        auto* parent = Parent();
+        auto replacement = FromArray(prefix_, json_, is_last_, depth_);
+        replacement->OnEvent(Event::ArrowRight);
+        parent->DetachAllChildren();  // Detach this.
+        parent->Add(replacement);
+      });
+      components.push_back(expand_button_);
+
+      std::map<std::string, int> columns_index;
+      for (auto& row : json_.items()) {
+        std::vector<Component> components_row;
+        children_.push_back({});
+        auto& children_row = children_.back();
+        for (auto& cell : row.value().items()) {
+          // Does it require a new column?
+          if (!columns_index.count(cell.key())) {
+            columns_index[cell.key()] = columns_.size();
+            columns_.push_back(cell.key());
+          }
+
+          // Does the current row fits in the current column?
+          if (children_row.size() <= columns_index[cell.key()])
+            children_row.resize(columns_index[cell.key()] + 1);
+
+          // Fill in the data
+          auto child = From(cell.value(), /*is_last=*/true, depth_ + 1);
+          children_row[columns_index[cell.key()]] = child;
+          components_row.push_back(child);
+        }
+        components.push_back(Container::Horizontal(std::move(components_row)));
+      }
+
+      Add(Container::Vertical(std::move(components)));
+    }
+
+   private:
+    Element Render() override {
+      std::vector<std::vector<Element>> data;
+      data.push_back({text("") | color(Color::GrayDark)});
+      for (auto& title : columns_)
+        data.back().push_back(text(title));
+      int i = 0;
+      for (auto& row_children : children_) {
+        std::vector<Element> data_row;
+        data_row.push_back(text(std::to_string(i++)) | color(Color::GrayDark));
+        for (auto& child : row_children) {
+          if (child) {
+            data_row.push_back(child->Render());
+          } else {
+            data_row.push_back(text(""));
+          }
+        }
+        data.push_back(std::move(data_row));
+      }
+      auto table = Table(std::move(data));
+      table.SelectColumns(1, -1).SeparatorVertical(LIGHT);
+      table.SelectColumns(1, -1).Border(LIGHT);
+      table.SelectRectangle(1, -1, 0, 0).SeparatorVertical(HEAVY);
+      table.SelectRectangle(1, -1, 0, 0).Border(HEAVY);
+
+      return vbox({
+          hbox({
+              prefix_->Render(),
+              expand_button_->Render(),
+          }),
+          table.Render(),
+      });
+    }
+
+    std::vector<std::string> columns_;
+    std::vector<std::vector<Component>> children_;
+
+    Component prefix_;
+    Component expand_button_;
+    const JSON& json_;
+    bool is_last_;
+    int depth_;
+  };
+
+  return Make<Impl>(prefix, json, is_last, depth);
+}
+
 
 Component Basic(std::string value, Color c, bool is_last) {
   return Renderer([value, c, is_last](bool focused) {
